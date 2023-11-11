775d83eb91def4a0002ffccc43897739d55b3b24
Lovre Crncevic <crncevic@f34r-mac.local>
2023-11-11 20:08:12 +0100

    Updates

---
+++ precache-manifest.cbcc47b2257a9c3e992d3317d7c6cb1f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b4b93abb2e14893bb007ec8b58b067",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "48e315cdce11f8226a68",
+    "url": "/portfolio/static/css/2.a4f3c679.chunk.css"
+  },
+  {
+    "revision": "2ac047c1188297ff8a15",
+    "url": "/portfolio/static/css/main.352cbeda.chunk.css"
+  },
+  {
+    "revision": "48e315cdce11f8226a68",
+    "url": "/portfolio/static/js/2.cb2e7756.chunk.js"
+  },
+  {
+    "revision": "4b779d99c235138e8fcf",
+    "url": "/portfolio/static/js/3.531994d3.chunk.js"
+  },
+  {
+    "revision": "2ac047c1188297ff8a15",
+    "url": "/portfolio/static/js/main.801de62c.chunk.js"
+  },
+  {
+    "revision": "b017c141ec3b24147dc0",
+    "url": "/portfolio/static/js/runtime~main.00bc68c8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
