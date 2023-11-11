86dea666b8c58e9b6db6c3c23cf75fd2eb2f5d88
Lovre Crncevic <crncevic@f34r-mac.local>
2023-11-11 20:35:18 +0100

    removed personal portfolio

---
+++ build/precache-manifest.28659830dead15d9a6c17ef7ed3a7359.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b545d705ae840529e0f02a45dcacca80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "722f6f8beeb484ed7b02",
+    "url": "/static/css/2.f54f84d0.chunk.css"
+  },
+  {
+    "revision": "c4bac43c7c9a01ff67b2",
+    "url": "/static/css/main.352cbeda.chunk.css"
+  },
+  {
+    "revision": "722f6f8beeb484ed7b02",
+    "url": "/static/js/2.cb2e7756.chunk.js"
+  },
+  {
+    "revision": "4b779d99c235138e8fcf",
+    "url": "/static/js/3.531994d3.chunk.js"
+  },
+  {
+    "revision": "c4bac43c7c9a01ff67b2",
+    "url": "/static/js/main.66e2de48.chunk.js"
+  },
+  {
+    "revision": "8d32b3028075a2d11385",
+    "url": "/static/js/runtime~main.ffe7d8dc.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
