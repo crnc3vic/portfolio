2b96f6e71fe496a85dc3a2a649e7c009c8283888
Lovre Crncevic <crncevic@f34r-mac.local>
2023-11-12 14:58:00 +0100

    Added cname support

--- scripts/create-cname.js
+++ scripts/create-cname.js
@@ -1,19 +1,21 @@
-
 const fs = require('fs');
-const url = require('url');
+const path = require('path');
 
-const packageJson = require('../package.json');
+const packageJsonPath = path.resolve(__dirname, '../package.json');
+const packageJson = require(packageJsonPath);
 const homepage = packageJson.homepage;
 
 if (homepage) {
   const parsedUrl = new URL(homepage);
   const domain = parsedUrl.hostname;
   
-  if (!fs.existsSync('../build')) {
-    fs.mkdirSync('../build');
+  const buildDir = path.resolve(__dirname, '../build');
+  
+  if (!fs.existsSync(buildDir)) {
+    fs.mkdirSync(buildDir);
   }
   
-  fs.writeFileSync('../build/CNAME', domain);
+  fs.writeFileSync(path.join(buildDir, 'CNAME'), domain);
 } else {
   console.error('Homepage is not defined in package.json!');
 }
